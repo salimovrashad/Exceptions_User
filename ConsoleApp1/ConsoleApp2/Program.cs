99dce7cc775de4a8ecb9121d49166367d080e15e
salimovrashad <selimovresad33@gmail.com>
2023-11-03 05:30:49 +0400

    add task

---
+++ ConsoleApp1/ConsoleApp2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Reflection;
+
+namespace ConsoleApp2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            var field = user.GetType().GetField("Age", System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Instance);
+            field.SetValue(user, 20);
+
+
+            Console.WriteLine("New Age: " + field.GetValue(user));
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp2/User.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    public class User
+    {
+        private int Age = 12;
+        private string Name = "Rashad";
+        private int Id = 1;
+    }
+}
