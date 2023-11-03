e130534ddc2d1b0bbd2aa29f9bf31781230be30d
salimovrashad <selimovresad33@gmail.com>
2023-11-03 05:46:19 +0400

    add task

---
+++ Reflection/Reflection/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace Reflection
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            User user = new User();
+
+            var field = user.GetType().GetField("Age", System.Reflection.BindingFlags.NonPublic
+                | System.Reflection.BindingFlags.Instance);
+            field.SetValue(user, 20);
+
+
+            Console.WriteLine("New Age: " + field.GetValue(user));
+        }
+    }
+}
---
+++ Reflection/Reflection/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Reflection
+{
+    public class User
+    {
+        private int Age = 12;
+        private string Name = "Rashad";
+        private int Id = 1;
+    }
+}
